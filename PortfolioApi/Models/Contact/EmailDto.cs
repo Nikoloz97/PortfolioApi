631524ae2b53ff2436a0a700bc3a09b889f79a16
Nikoloz97 <nick.gotsy@gmail.com>
2023-10-23 20:36:54 -0400

    EmailService now sends email via SendGrid

--- PortfolioApi/Models/Contact/EmailDto.cs
+++ PortfolioApi/Models/Contact/EmailDto.cs
@@ -5,7 +5,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }    
-        public string Category { get; set; }
         public string Subject { get; set; }   
         public string Body { get; set; }    
         
--- PortfolioApi/Services/Email/EmailService.cs
+++ PortfolioApi/Services/Email/EmailService.cs
@@ -2,13 +2,19 @@
 using System.Net.Mail;
 using System.Net;
 using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
 
 namespace PortfolioApi.Services.Email
 {
     public class EmailService  : IEmailService
     {
         private readonly IConfiguration _configuration;
-        private string _mailTo = "nikolectures@gmail.com";
+        // TODO: Make _mailFrom a contact email service (e.g. NickPortfolioEmailService@gmail.com) 
+        private string _mailFrom = "nikolectures@gmail.com";
+
 
         public EmailService(IConfiguration configuration)
         {
@@ -17,10 +23,32 @@ namespace PortfolioApi.Services.Email
 
         public async Task SendEmailAsync(EmailDto email)
         {
-            // Email sending logic here
-            var apikey = _configuration.GetSection("MyApiSettings").GetValue<string>("ApiKey");
+            // TODO: move apikey to azure keyvault 
+            var apiKey = _configuration.GetSection("MyApiSettings").GetValue<string>("ApiKey");
 
-            Console.WriteLine(apikey);
+            var client = new SendGridClient(apiKey);
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_mailFrom, email.FirstName + " " + email.LastName),
+                Subject = email.Subject,
+
+                // TODO: pick one of these properties for body? 
+                // TODO: Format contact email within the Content
+                PlainTextContent = email.Body + " " + email.EmailAddress,
+                HtmlContent = email.Body + " " + email.EmailAddress
+            };
+
+            msg.AddTo(new EmailAddress("nick.gotsy@gmail.com"));
+
+            var response = await client.SendEmailAsync(msg);
+
+            // TODO: fix error message (see Exceptions folder) 
+            if (response.StatusCode != HttpStatusCode.Accepted)
+            {
+                Console.WriteLine("The email did not go through...");
+                Console.WriteLine(response.StatusCode);
+            }
         }
     }
 }
