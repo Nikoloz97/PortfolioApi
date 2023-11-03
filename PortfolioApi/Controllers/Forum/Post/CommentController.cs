faf8ac704977bd7833a04ed10611fe43c6deaa32
Nikoloz97 <nick.gotsy@gmail.com>
2023-11-02 23:02:34 -0400

    Created test endpoint

--- PortfolioApi/Controllers/Forum/Post/CommentController.cs
+++ PortfolioApi/Controllers/Forum/Post/CommentController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioApi.Models.User;
 
 namespace PortfolioApi.Controllers.Forum.Post
 {
-    [Route("api/[controller]")]
+    [Route("api/forum/post/comment")]
     [ApiController]
     public class CommentController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            return Ok("Tacos");
+        }
     }
 }
