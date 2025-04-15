2b4080f030e0429137a67573e3291677fec2ce10
Nikoloz97 <nick.gotsy@gmail.com>
2025-04-15 18:28:55 -0400

    Cors now allows credentials for cookies

--- PortfolioApi/Program.cs
+++ PortfolioApi/Program.cs
@@ -18,71 +18,89 @@ using PortfolioApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddControllers();
-
 builder.Services.AddAuthorization();
-
 builder.Services.AddSwaggerGen();
+
+// Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSecretKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token = context.Request.Cookies["auth_token"];
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 builder.Services.AddDbContext<UserContext>(options =>
       options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"],
       sqlServerOptionsAction: sqlOptions =>
       {
           sqlOptions.EnableRetryOnFailure(
-              maxRetryCount: 5, 
-              maxRetryDelay: TimeSpan.FromSeconds(15), 
-              errorNumbersToAdd: null 
+              maxRetryCount: 5,
+              maxRetryDelay: TimeSpan.FromSeconds(15),
+              errorNumbersToAdd: null
           );
       }
       ));
 
 /* Repositories */
-
 builder.Services.AddScoped<IForumRepository, ForumRepository>();
-
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
 builder.Services.AddScoped<IGeoGameRepository, GeoGameRepository>();
 
-
 /* Services */
-
 builder.Services.AddScoped<IForumProfileService, ForumProfileService>();
-
 builder.Services.AddScoped<IGeoGameService, GeoGameService>();
-
 builder.Services.AddScoped<IUserService, UserService>();
 
-builder.Services.AddScoped<IEmailService, EmailService>();
+// Sendgrid
+builder.Services.AddSingleton<IEmailService>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    return new EmailService(configuration);
+});
+
+// AzureStorage
+builder.Services.AddScoped<IAzureStorageService>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var connectionString = configuration["AzureStorageConnectionString"];
+    var containerName = configuration["AzureStorage:ProfileImagesContainer"];
+    return new AzureStorageService(connectionString, containerName);
+});
 
 builder.Services.AddScoped<IAuthService, AuthService>();
-
-
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+builder.Services.AddCors(options => options.AddPolicy("corsapp", policy =>
 {
-builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    policy
+        .WithOrigins("http://localhost:3000", "https://nicksportfolio.azurewebsites.net")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
 }));
 
 builder.Services.AddSingleton(builder.Configuration);
 
 /* Misc */
-
-builder.Services.AddScoped<IAzureStorageService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var connectionString = configuration["AzureStorage:ConnectionString"];
-    var containerName = configuration["AzureStorage:ProfileImagesContainer"];
-    return new AzureStorageService(connectionString, containerName);
-});
-
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-
 
 // Enforce HTTPS
 builder.Services.AddHttpsRedirection(options =>
@@ -114,61 +132,17 @@ if (app.Environment.IsDevelopment())
 }
 
 /* Middleware */
-
 app.UseMiddleware<ExceptionMiddleware>();
 
 if (!app.Environment.IsDevelopment())
 {
     // Enforced HTTPS
-    app.UseHsts(); 
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-// Authorization Middleware
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSecretKey"])),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero
-        };
-
-        options.Events = new JwtBearerEvents
-        {
-            OnMessageReceived = context =>
-            {
-                context.Token = context.Request.Cookies["auth_token"];
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-// Sendgrid
-builder.Services.AddSingleton<IEmailService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    return new EmailService(configuration);
-});
-
-// AzureStorage
-builder.Services.AddScoped<IAzureStorageService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var connectionString = configuration["AzureStorageConnectionString"];
-    var containerName = configuration["AzureStorage:ProfileImagesContainer"];
-    return new AzureStorageService(connectionString, containerName);
-});
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
